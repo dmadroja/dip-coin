a4af1915ec285659e85bf138de6c9c0c788c3579
Dipen Adroja <dmadroja@gmail.com>
2019-01-14 08:42:02 +0530

    Created blockchain network

---
+++ app/p2p-server.js
@@ -0,0 +1,57 @@
+const Websocket = require("ws");
+
+const P2P_PORT = process.env.P2P_PORT || 5001;
+const peers = process.env.PEERS ? process.env.PEERS.split(',') : [];
+
+class P2pServer {
+	constructor(blockchain) {
+		this.blockchain = blockchain;
+		this.sockets = [];
+	}
+
+	listen() {
+
+		const server = new Websocket.Server({
+			port: P2P_PORT
+		});
+		server.on('connection', socket => {
+			this.connectSocket(socket);
+		});
+		this.connectToPeers();
+		console.log(`Listening for peer to peer connection on: ${P2P_PORT}`);
+	}
+
+	connectSocket(socket) {
+		this.sockets.push(socket);
+		console.log('Socket connected');
+		this.messageHandler(socket);
+		this.sendChain(socket);
+	}
+
+	connectToPeers() {
+		peers.forEach(peer => {
+			//ws://localhost:5001
+			const socket = new Websocket(peer);
+			socket.on('open', () => this.connectSocket(socket));
+		});
+	}
+
+	messageHandler(socket) {
+		socket.on('message', message => {
+			const data = JSON.parse(message);
+			this.blockchain.replaceChain(data);
+		});
+	}
+
+	sendChain(socket) {
+		socket.send(JSON.stringify(this.blockchain.chain));
+	}
+
+	syncChains() {
+		this.sockets.forEach(socket => {
+			this.sendChain(socket);
+		});
+	}
+}
+
+module.exports = P2pServer;
