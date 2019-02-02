6322f9cff90afcc7397f1738e40aac455f39cacd
Dipen Adroja <dmadroja@gmail.com>
2019-02-02 13:35:37 +0530

    Added wallet and transaction with signature feature

---
+++ chain-util.js
@@ -0,0 +1,21 @@
+const EC = require('elliptic').ec;
+const uuidV1 = require('uuid/v1');
+const ec = new EC('secp256k1');
+const SHA256 = require('crypto-js/sha256');
+
+class ChainUtil{
+
+	static genKeyPair() {
+		return ec.genKeyPair();		
+	}
+
+	static id() {
+		return uuidV1();
+	}
+
+	static hash(data) {
+		return SHA256(JSON.stringify(data)).toString();
+	}
+}
+
+module.exports = ChainUtil;
---
+++ wallet/transaction.test.js
@@ -0,0 +1,33 @@
+const Transaction = require('./transaction');
+const Wallet = require('./index');
+
+
+describe('Transaction', () => {
+	let transaction, wallet, recipient, amount;
+
+	beforeEach(() => {
+		wallet = new Wallet();
+		amount = 50;
+		recipient = 'r3c1p13nt';
+		transaction = Transaction.newTransaction(wallet, recipient, amount);
+	});
+
+	it('outputs the `amount` substracted from the wallet balance', () => {
+		expect(transaction.outputs.find(output => output.address === wallet.publicKey).amount).toEqual(wallet.balance - amount);
+	});
+
+	it('outputs the `amount` added to the recipient', () => {
+		expect(transaction.outputs.find(output => output.address === recipient.publicKey).amount).toEqual(amount);
+	});
+
+	describe('transact with amount which exceed amount balance', () => {
+		beforeEach(() => {
+			amount = 500000;
+			transaction = Transaction.newTransaction(wallet, recipient, amount);
+		})
+
+		it('does not create transaction', () => {
+			expect(transaction).toEqual(undefined);
+		})
+	})
+});
